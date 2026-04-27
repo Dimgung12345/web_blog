2a56d78b9f8dfa6f6d809d23b26442ac3bfc4f33
Luthfi Malik <_@upi.web.id>
2026-04-27 19:39:47 +0700

    Prefix all blog endpoints with /blog

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import pageRoutes from "./routes/pageRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const BASE_PATH = "/blog";
 
 // utils untuk file
 const __filename = fileURLToPath(import.meta.url);
@@ -23,19 +24,19 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/storage", express.static("storage"));
+app.use(`${BASE_PATH}/storage`, express.static("storage"));
 // utils ssr
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // Page Routes (SSR)
-app.use("/", pageRoutes);
+app.use(BASE_PATH, pageRoutes);
 
 // API routes
-app.use("/posts", postRoutes);
-app.use("/users", userRoutes);
-app.use("/media", mediaRoutes);
-app.use("/categories", categoryRoutes);
+app.use(`${BASE_PATH}/posts`, postRoutes);
+app.use(`${BASE_PATH}/users`, userRoutes);
+app.use(`${BASE_PATH}/media`, mediaRoutes);
+app.use(`${BASE_PATH}/categories`, categoryRoutes);
 
 // 404 not found kalo gada
 app.use((req, res) => {
