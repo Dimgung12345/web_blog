c8f0b78d61cf6fbb7d8444cb34cd3f051d4a5155
upi mau kerja <kaplingtumbal@gmail.com>
2026-07-08 19:26:34 +0700

    Pagination. Implemented (#19)

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -5,10 +5,10 @@ import { sessionMiddleware } from "../middleware/session.js";
 const router = express.Router();
 
 router.get("/", postController.getAllPosts);
-router.get("/:id", postController.getPostById);
 router.get("/search", postController.searchPosts);
-router.get("/:slug", postController.getPostBySlug);
 router.get("/category/:categoryId", postController.getPostByCategory);
+router.get("/:id", postController.getPostById);
+router.get("/:slug", postController.getPostBySlug);
 router.post("/", sessionMiddleware, postController.createPost);
 router.put("/:id", sessionMiddleware, postController.updatePost);
 router.delete("/:id", sessionMiddleware, postController.deletePost);
