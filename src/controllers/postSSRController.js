47a5acc358d3bf0deccca3d0fe9510396bab1881
upi mau kerja <kaplingtumbal@gmail.com>
2026-07-08 19:27:38 +0700

    eBlog-RPL -> Blog (#20)
    Co-authored-by: Atalaa <athallahrikza2@gmail.com>

--- src/controllers/postSSRController.js
+++ src/controllers/postSSRController.js
@@ -107,7 +107,7 @@ export const getAllPosts = async (req, res) => {
             posts,
             categories,
             isLandingPage: false,
-            title: "Semua Artikel - eBlog-RPL",
+            title: "Semua Artikel - Blog"
         });
     } catch (err) {
         res.status(500).render("pages/public/404");
